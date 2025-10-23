11c313c0aabf1ec919e3b51ca2e385f4863396c1
t01kessl <thomas01.kessler@iav.de>
2025-10-23 17:31:49 +0200

    Playlist überarbeitet. Nummer spinner zur Auswahl eingeführt. Scheduler erstmalig umgesetzt, so dass zu der vorgegebenen Zeit ein jingle abspielbar ist.

--- app/src/main/java/com/example/jingleplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/jingleplayerapp/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.jingleplayerapp
 import android.content.Context
 import android.util.Log
 import android.database.Cursor
-import android.os.Build
 import android.os.Bundle
 import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,35 +14,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import java.time.Duration
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-
 import androidx.compose.material3.FloatingActionButton
-
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-
 import androidx.compose.material3.Scaffold
-
 import androidx.compose.material3.Text
-
 import android.media.MediaPlayer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.TextField
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
+
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -52,23 +44,22 @@ import androidx.compose.ui.unit.dp
 import com.example.jingleplayerapp.ui.theme.JingleplayerappTheme
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.ui.text.input.KeyboardType
-import kotlinx.coroutines.delay
+import androidx.compose.runtime.setValue
+
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.Duration
 
 
-data class Jingle(val name: String, val type: String, val start: LocalDateTime)
-data class Game(val name: String,  val start: LocalDateTime,val end: LocalDateTime)
 
-data class Nextsong(val name:String, val type: String, val duration: Duration)
+data class Jingle(val name: String, val type: String, val start: Instant)
+data class Game(val name: String,  val start: Instant,val end: Instant)
+
+data class Songtoplay(val name:String, val type:String)
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
-    lateinit var mediaPlayer: MediaPlayer
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -78,16 +69,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onDestroy() {
         super.onDestroy()
     }
 }
 
 @Composable
-@RequiresApi(Build.VERSION_CODES.O)
+
 fun Mainmenu(context: Context) {
-    var delayminutes = remember { mutableStateOf("5") }
+    val delayminutes = remember { mutableIntStateOf(5) }
     val jingleslist = remember { mutableStateListOf<Jingle>() }
     val gameslist =  remember {mutableStateListOf<Game>()}
     Scaffold(
@@ -102,10 +93,10 @@ fun Mainmenu(context: Context) {
                     })
         },
         bottomBar = {
-            var nextsong = remember {mutableStateOf<Nextsong>(Nextsong("","Start", Duration.ZERO))}
+            val songtoplay = remember {mutableStateOf<Songtoplay>(Songtoplay("","Start"))}
             BottomAppBar(
                 content = {
-                    FindNextSong(jingleslist,nextsong)
+                    FindNextSong(context,jingleslist,songtoplay)
                 })
 
         },
@@ -118,7 +109,7 @@ fun Mainmenu(context: Context) {
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            ShowCalenderEvents(gameslist)
+            // ShowCalenderEvents(gameslist)
             CreatePlayList(gameslist,jingleslist,delayminutes)
         }
     }
@@ -134,7 +125,7 @@ fun FAB() {
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
+
 @Composable
 fun LoadCalenderEvents(gameslist:MutableList<Game>) {
     Log.i("LoadCalenderEvents", "Fire Composable")
@@ -150,8 +141,8 @@ fun LoadCalenderEvents(gameslist:MutableList<Game>) {
         mutableStateOf(0)
     }
 
-    var calnames =  remember {mutableListOf<String>("Select Calender")}
-    var calmap =  remember {mutableMapOf<String, Long>()}
+    val calnames =  remember {mutableListOf<String>("Select Calender")}
+    val calmap =  remember {mutableMapOf<String, Long>()}
 
 
 
@@ -192,8 +183,8 @@ fun LoadCalenderEvents(gameslist:MutableList<Game>) {
                     )
                     calendarscursor?.apply {
                         while (moveToNext()) {
-                            var displayname = getString(PROJECTION_DISPLAY_NAME_INDEX)
-                            var id = getLong(PROJECTION_ID_INDEX)
+                            val displayname = getString(PROJECTION_DISPLAY_NAME_INDEX)
+                            val id = getLong(PROJECTION_ID_INDEX)
                             calnames.add(displayname)
                             calmap.put(displayname, id)
                         }
@@ -239,9 +230,9 @@ fun LoadCalenderEvents(gameslist:MutableList<Game>) {
                             val EVENTPROJECTION_DTEND_INDEX = 2
                             // val EVENTPROJECTION_TZ_INDEX= 3
                             // val EVENTPROJECTION_TZEND_INDEX= 4
-                            var calId = calIdstate.value.toString()
+                            val calId = calIdstate.value.toString()
                             val urievent = CalendarContract.Events.CONTENT_URI
-                            val selectionevent: String = "${CalendarContract.Events.CALENDAR_ID}=${calId}"
+                            val selectionevent = "${CalendarContract.Events.CALENDAR_ID}=${calId}"
                             val eventscursor: Cursor? = context.contentResolver.query(
                                 urievent,
                                 EVENTPROJECTION,
@@ -270,14 +261,10 @@ fun LoadCalenderEvents(gameslist:MutableList<Game>) {
                                             //val eventtzend = getString(EVENTPROJECTION_TZEND_INDEX)
 
                                             val eventstartdatetime = Instant.ofEpochMilli(eventstart)
-                                                //.atZone(ZoneId.of(eventtz))
-                                                .atZone(ZoneId.systemDefault())
-                                                .toLocalDateTime()
+
 
                                             val eventenddatetime = Instant.ofEpochMilli(eventend)
-                                                //.atZone(ZoneId.of(eventtzend))
-                                                .atZone(ZoneId.systemDefault())
-                                                .toLocalDateTime()
+
                                             val game = Game(eventtitle, eventstartdatetime, eventenddatetime)
                                             gameslist.add(game)
                                         }
@@ -302,141 +289,132 @@ fun LoadCalenderEvents(gameslist:MutableList<Game>) {
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
+
+
+
 @Composable
-// https://stackoverflow.com/questions/76465858/how-to-access-state-from-another-composable-in-jetpack-compose
-fun ShowCalenderEvents(gameslist:MutableList<Game>) {
 
+fun CreatePlayList(gameslist: List<Game>, playlist: MutableList<Jingle>, delayminutes:MutableState<Int>) {
 
-            Text(
-            text = "Game Events",
-            modifier = Modifier.padding(16.dp)
-        )
-        LazyColumn( modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.3f) ) {
-            items(gameslist) { game ->
-                Text(text = game.name + ":" + game.start, modifier = Modifier.padding(8.dp))
-            }
-        }
-
-    }
-
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun CreatePlayList(gameslist: List<Game>, jingleslist: MutableList<Jingle>, delayminutes:MutableState<String>) {
     Log.i("CreatePlayList","Create Jingle list with ${gameslist.size} game(s) and delay of ${delayminutes}" )
-    jingleslist.clear()
-    for (index in gameslist.indices) {
-        val game: Game = gameslist[index]
-        val jinglestart = Jingle(game.name, "Start", game.start)
-        val jingleend = Jingle(game.name, "End", game.end)
-        val enventdeltaenddatetime =
-            game.end.minusMinutes(delayminutes.value.toLong())
-        val jingledeltaend =
-            Jingle(game.name, "DeltaEnd", enventdeltaenddatetime)
-        jingleslist.add(jinglestart)
-        jingleslist.add(jingleend)
-        if (delayminutes.value.toLong() > 0) {
-            jingleslist.add(jingledeltaend)
-        }
+
+
+     //   LaunchedEffect(Unit) {
+       //     while (true) {
+                playlist.clear()
+                for (index in gameslist.indices) {
+                    val now = Instant.now()
+                    val game: Game = gameslist[index]
+
+                    val jinglestart = Jingle(game.name, "Start", game.start)
+                    val jingleend = Jingle(game.name, "End", game.end)
+                    val enventdeltaenddatetime =
+                        game.end.minusSeconds(delayminutes.value.toLong() * 60)
+                    val jingledeltaend =
+                        Jingle(game.name, "DeltaEnd", enventdeltaenddatetime)
+
+                    if (now.isBefore(jinglestart.start)) {
+                        playlist.add(jinglestart)
+                    }
+                    if (now.isBefore(jingleend.start)) {
+                        playlist.add(jingleend)
+                    }
+                    if (now.isBefore(jingledeltaend.start)) {
+                        if (delayminutes.value.toLong() > 0) {
+                            playlist.add(jingledeltaend)
+                        }
+                    }
+                    playlist.sortBy { it.start }
+                    Log.i("CreatePlayList", "${playlist.size} jingles added to Playlist")
+                    // delay(10000)
+
+                }
+
+
+     //       }
+     //   }
+    Box(modifier = Modifier.fillMaxSize(0.2f)) {
+        NumberPicker(
+            // state = remember { mutableStateOf(5) },
+            state =  delayminutes ,
+            range = 0..30,
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
-    // sort by Start of jingle
-    jingleslist.sortBy { it.start}
-    Log.i("CreatePlayList","${jingleslist.size} jingles added to Playlist")
-    TextField(
-        value = delayminutes.value,
-        onValueChange = { newValue ->
-            // Ensure the input is an integer
-            if (newValue.toIntOrNull() != null || newValue.isEmpty()) {
-                delayminutes.value = newValue
-            }
-        },
-        label = { Text("Minutes before end to play jingle") },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-    )
+
     Text(
-        text = "Playlist",
-        modifier = Modifier.padding(16.dp)
+        text = "Songs still to play:",
+        // modifier = Modifier.padding(16.dp)
     )
     LazyColumn( modifier = Modifier
         .fillMaxWidth()
-        .fillMaxHeight(0.3f)
+        .fillMaxHeight(0.9f)
          ) {
-        items(jingleslist) { jingle ->
-            Text(text = jingle.name + " " + jingle.type + " "+ jingle.start, modifier = Modifier.padding(8.dp))
+        items(playlist) { jingle ->
+            Text(text = jingle.name + " " + jingle.type + " "+ LocalDateTime.ofInstant(jingle.start,
+               ZoneId.systemDefault()), modifier = Modifier.padding(8.dp))
         }
 
     }
 }
 
 
-@RequiresApi(Build.VERSION_CODES.O)
+
 @Composable
-fun FindNextSong(jingleslist: List<Jingle>, nextsong: MutableState<Nextsong>) {
-    Log.i("FindNextSong","Starting Scheduler with ${jingleslist.size} jingles" )
+fun FindNextSong(context:Context, playlist: List<Jingle>, nextsong: MutableState<Songtoplay>) {
+    Log.i("FindNextSong", "Starting Scheduler with ${playlist.size} jingles")
+    val songtoplay = remember {mutableStateOf<Songtoplay>(Songtoplay("","Start"))}
+    var textit by remember { mutableStateOf("") }
+    //LaunchedEffect(Unit) {
+    //    while(true){
+            if (playlist.isNotEmpty()) {
+                val now = Instant.now()
+                val nextjingle = playlist.first()
+                val duration =Duration.between(now,nextjingle.start)
+                nextsong.value = Songtoplay(nextjingle.name, nextjingle.type)
+                textit = "${nextjingle.name}/${nextjingle.type}: Start in ${duration.toMinutes()} min."
+                Log.i(
+                    "LaunchedEffect",
+                    "First jingle with positive duration: ${nextjingle.name}, Time to start: ${duration.toMinutes()} minutes"
+                )
+                // wenn die playlist einen weiterspringt
+                if (songtoplay.value != nextsong.value){
+                // spiele den song davor einmal ab
+                    PlaySound(context,songtoplay.value)
+                songtoplay.value=nextsong.value
+                }
+            } else {
+                Log.i("LaunchedEffect", "No jingle with positive duration found.")
+                textit = "No jingles left"
+            }
+            
+      //      delay(10000)
+      //  }
+    //}
+        Text(
+            text = textit,
+            modifier = Modifier.padding(16.dp)
+        )
 
-    val now = LocalDateTime.now()
-
-    val firstPositiveDurationJingle = jingleslist
-        .map { jingle ->
-            val duration = Duration.between(now, jingle.start)
-            jingle to duration
-        }
-        .firstOrNull { (_, duration) -> !duration.isNegative }
-    var textit = ""
-    if (firstPositiveDurationJingle != null) {
-        val (nextjingle, nextjingleduration) = firstPositiveDurationJingle
-
-        nextsong.value=Nextsong(nextjingle.name,nextjingle.type,nextjingleduration)
-        textit = "${nextjingle.name}: Start in ${nextjingleduration.toMinutes()} min"
-        Log.i("FindNextSong","First jingle with positive duration: ${nextjingle.name}, Time to start: ${nextjingleduration.toMinutes()} minutes")
-    } else {
-        Log.i("FindNextSong","No jingle with positive duration found.")
-        textit = "No more jingles"
-    }
-    Text(
-        text =textit ,
-        modifier = Modifier.padding(16.dp)
-    )
 }
 
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun Countdown(nextsong: Nextsong){
-    Log.i("countdown","Starting Countdown" )
-    val context = LocalContext.current
-    var isTimeReached =false
+fun PlaySound(context:Context,songtoplay: Songtoplay) {
 
-    LaunchedEffect(Unit) {
-        while (nextsong.duration.toMillis()>5000){
-            delay(1000)
-        }
-        isTimeReached=true
-    }
-    if(isTimeReached){PlaySound(context,nextsong)}
-}
 
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun PlaySound(context:Context,nextsong: Nextsong) {
-    // val context = LocalContext.current
-    // which sounds should be played
+    val mediaPlayer = remember { MediaPlayer.create(context, R.raw.sound) }
     val soundMap = mutableMapOf(
         "Start" to R.raw.sound,
         "End" to R.raw.soundend,
         "DeltaEnd" to R.raw.sound5min
     )
-    val songId = soundMap[nextsong.type]
-    if (songId != null) {
-        val mediaPlayer = remember { MediaPlayer.create(context, songId) }
+    val soundId=soundMap[songtoplay.type]
+    if (soundId!=null) {
+        val mediaPlayer = remember { MediaPlayer.create(context, soundId) }
         mediaPlayer.start()
-        Log.i("PlaySound", "Playing sound: ${nextsong.name}")
+        Log.i("PlaySound", "Playing ${songtoplay.type}-jingle for ${songtoplay.name} ")
     }
-    val mediaPlayer = remember { MediaPlayer.create(context, R.raw.sound) }
-    mediaPlayer.start()
-
     // Button to play sound
     /*
     IconButton(onClick = {mediaPlayer.start() }) {
@@ -467,4 +445,6 @@ fun PlaySound(context:Context,nextsong: Nextsong) {
         }
     }
 }
+
+
 
