50bfcc2183c6f67b493cdd8f81d1eae74d9b9106
t01kessl <thomas01.kessler@iav.de>
2025-10-24 03:39:04 +0200

    Audiofokus hinzugefügt für exoplayer.

--- app/src/main/java/com/example/jingleplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/jingleplayerapp/MainActivity.kt
@@ -1,7 +1,11 @@
 // icons from https://fonts.google.com/icons?selected=Material+Symbols+Outlined:stop:FILL@0;wght@400;GRAD@0;opsz@24&icon.size=24&icon.color=%231f1f1f&icon.platform=android
 package com.example.jingleplayerapp
-import android.util.Log
+
+import CalendarViewModel
+import Game
+import Jingle
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -11,8 +15,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -21,47 +30,32 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.jingleplayerapp.ui.theme.JingleplayerappTheme
-import androidx.compose.runtime.setValue
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.RawResourceDataSource
 import androidx.media3.exoplayer.ExoPlayer
-
+import com.example.jingleplayerapp.ui.theme.JingleplayerappTheme
+import kotlinx.coroutines.delay
+import java.time.Duration
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.Duration
-import androidx.media3.common.util.UnstableApi
-import androidx.media3.datasource.RawResourceDataSource
-import kotlinx.coroutines.delay
-import kotlin.Boolean
-import CalendarViewModel
-import Game
-import Jingle
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateListOf
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlin.collections.mutableListOf
 
 
 data class Songtoplay(val name:String, val type:String)
@@ -78,6 +72,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         exoPlayer = ExoPlayer.Builder(this).build()
+        // handle audiofocus
+        val audioAttributes: AudioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .build()
+        exoPlayer.setAudioAttributes(audioAttributes, true)
         setContent {
             JingleplayerappTheme {
                 Mainmenu(exoPlayer,calendarViewModel)
@@ -111,11 +111,9 @@ fun Mainmenu(exoPlayer: ExoPlayer, calendarViewModel: CalendarViewModel) {
                     })
         },
         bottomBar = {
-            val songtoplay = remember {mutableStateOf<Songtoplay>(Songtoplay("","Start"))}
-
             BottomAppBar(
                 content = {
-                    Scheduler(exoPlayer,jingleslist,songtoplay,jinglelength)
+                    Scheduler(exoPlayer,jingleslist,nextsong,jinglelength)
                 })
 
         },
@@ -184,8 +182,8 @@ fun CalendarDropdown(calendarViewModel: CalendarViewModel) {
         }
     }
 }
-@Composable
 
+@Composable
 fun CreatePlayList(calendarViewModel: CalendarViewModel, playlist: MutableList<Jingle>, delayminutes:MutableState<Int>) {
     val uiState by calendarViewModel.uiState.collectAsState()
     val gameslist = uiState.games
